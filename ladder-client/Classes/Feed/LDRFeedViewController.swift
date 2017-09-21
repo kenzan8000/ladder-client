99ca3794deac1642a805c13b51e37894c13788a5
kenzan8000 <kenzan8000@gmail.com>
2017-09-21 01:04:47 -0700

    add rate or folder switch button on feed view controller

--- ladder-client/Classes/Feed/LDRFeedViewController.swift
+++ ladder-client/Classes/Feed/LDRFeedViewController.swift
@@ -6,24 +6,35 @@ class LDRFeedViewController: UIViewController {
 
     // MARK: - properties
 
+    @IBOutlet weak var segmentedControl: UISegmentedControl!
+
 
     // MARK: - life cycle
 
     override func loadView() {
         super.loadView()
 
+        // segmented control
+        self.segmentedControl.setImage(
+            IonIcons.image(withIcon: ion_ios_star_outline, iconColor: UIColor.gray, iconSize: 32, imageSize: CGSize(width: 32, height: 32)),
+            forSegmentAt: 0
+        )
+        self.segmentedControl.setImage(
+            IonIcons.image(withIcon: ion_ios_folder_outline, iconColor: UIColor.gray, iconSize: 32, imageSize: CGSize(width: 32, height: 32)),
+            forSegmentAt: 1
+        )
         // bar button items
         self.navigationItem.leftBarButtonItem = UIBarButtonItem(
             image: IonIcons.image(withIcon: ion_ios_reload, iconColor: UIColor.gray, iconSize: 32, imageSize: CGSize(width: 32, height: 32)),
             style: .plain,
             target: self,
-            action: #selector(LDRFeedViewController.touchedUpInside)
+            action: #selector(LDRFeedViewController.barButtonItemTouchedUpInside)
         )
         self.navigationItem.rightBarButtonItem = UIBarButtonItem(
             image: IonIcons.image(withIcon: ion_ios_gear_outline, iconColor: UIColor.gray, iconSize: 32, imageSize: CGSize(width: 32, height: 32)),
             style: .plain,
             target: self,
-            action: #selector(LDRFeedViewController.touchedUpInside)
+            action: #selector(LDRFeedViewController.barButtonItemTouchedUpInside)
         )
     }
 
@@ -38,8 +49,24 @@ class LDRFeedViewController: UIViewController {
 
     /// MARK: - event listener
 
-    func touchedUpInside(barButtonItem: UIBarButtonItem) {
+    /**
+     * called when value changed
+     * @param segmentedControl UISegmentedControl
+     **/
+    @IBAction func segmentedControlValueChanged(segmentedControl: UISegmentedControl) {
 
     }
+
+    /**
+     * called when touched up inside
+     * @param barButtonItem UIBarButtonItem
+     **/
+    func barButtonItemTouchedUpInside(barButtonItem: UIBarButtonItem) {
+        if barButtonItem == self.navigationItem.leftBarButtonItem {
+        }
+        else if barButtonItem == self.navigationItem.rightBarButtonItem {
+        }
+    }
+
 }
 
