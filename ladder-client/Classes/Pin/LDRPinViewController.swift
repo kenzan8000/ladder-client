3cc030ad2460def3fb13cde3a75b69a0e16569df
kenzan8000 <kenzan8000@gmail.com>
2017-09-22 12:44:58 -0700

    add navigation bar button items on pin view controller

--- ladder-client/Classes/Pin/LDRPinViewController.swift
+++ ladder-client/Classes/Pin/LDRPinViewController.swift
@@ -6,11 +6,40 @@ class LDRPinViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+
+        // bar button items
+        self.navigationItem.leftBarButtonItem = UIBarButtonItem(
+            image: IonIcons.image(withIcon: ion_ios_reload, iconColor: UIColor.darkGray, iconSize: 32, imageSize: CGSize(width: 32, height: 32)),
+            style: .plain,
+            target: self,
+            action: #selector(LDRPinViewController.barButtonItemTouchedUpInside)
+        )
+        self.navigationItem.rightBarButtonItem = UIBarButtonItem(
+            image: IonIcons.image(withIcon: ion_ios_gear_outline, iconColor: UIColor.darkGray, iconSize: 32, imageSize: CGSize(width: 32, height: 32)),
+            style: .plain,
+            target: self,
+            action: #selector(LDRPinViewController.barButtonItemTouchedUpInside)
+        )
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
     }
+
+
+    /// MARK: - event listener
+
+    /**
+     * called when touched up inside
+     * @param barButtonItem UIBarButtonItem
+     **/
+    func barButtonItemTouchedUpInside(barButtonItem: UIBarButtonItem) {
+        if barButtonItem == self.navigationItem.leftBarButtonItem {
+        }
+        else if barButtonItem == self.navigationItem.rightBarButtonItem {
+        }
+    }
+
 
 }
 
