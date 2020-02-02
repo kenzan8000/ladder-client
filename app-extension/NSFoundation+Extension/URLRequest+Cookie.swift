6a519ff957f66b822594c5cd64bd5bdf8a280f22
Kenzan Hase <kenzan8000@gmail.com>
2020-02-01 23:37:20 -0800

    add URLRequest+Cookie

---
+++ app-extension/NSFoundation+Extension/URLRequest+Cookie.swift
@@ -0,0 +1,24 @@
+import Foundation
+
+
+/// MARK: - MutableURLRequest+Cookie
+extension MutableURLRequest {
+
+    /// MARK: - public api
+
+    /**
+     * set cookies
+     */
+    func setCookies() {
+        let cookies = HTTPCookieStorage.shared.cookies
+        if cookies == nil { return }
+
+        var value = ""
+        for cookie in cookies! {
+            if !(cookie.domain.hasSuffix(self.url!.host!)) { continue }
+            value = "\(value)\(cookie.name)=\(cookie.value);"
+        }
+        self.setValue(value, forHTTPHeaderField: "Cookie")
+    }
+
+}
