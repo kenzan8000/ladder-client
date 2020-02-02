6258f915e76bb6ec39b61a9610aefd4bf4c40095
Kenzan Hase <kenzan8000@gmail.com>
2020-02-01 23:09:08 -0800

    add HTTPCookieStorage+Cookie.swift

---
+++ app-extension/NSFoundation+Extension/HTTPCookieStorage+Cookie.swift
@@ -0,0 +1,98 @@
+import Foundation
+
+
+/// MARK: - HTTPCookieStorage+Cookie
+extension HTTPCookieStorage {
+
+    /// MARK: - public api
+
+    /**
+     * check if has the cookie
+     * @param name cookie name
+     * @param domain cookie domain
+     * @return ture or false
+     */
+    func hasCookie(name: String, domain: String) -> Bool {
+        var has = false
+
+        let cookies = HTTPCookieStorage.shared.cookies
+        if cookies == nil { return has }
+        for cookie in cookies! {
+            if name != cookie.name { continue }
+            if domain != cookie.domain { continue }
+            has = true
+            break
+        }
+
+        return has
+    }
+
+    /**
+     * add cookies by url response
+     * @param httpUrlResponse HTTPURLResponse
+     **/
+    func addCookies(httpUrlResponse: HTTPURLResponse?) {
+        if httpUrlResponse == nil { return }
+
+        var headerFields: [String: String] = [:]
+        for (key, value) in httpUrlResponse!.allHeaderFields {
+            if !(key is String) { continue }
+            if !(value is String) { continue }
+            headerFields[key as! String] = value as? String
+        }
+        if httpUrlResponse!.url == nil { return }
+        let cookies = HTTPCookie.cookies(withResponseHeaderFields: headerFields, for: httpUrlResponse!.url!)
+        for cookie in cookies {
+            HTTPCookieStorage.shared.setCookie(cookie)
+
+            let url = LDRUrl(path: LDR.login)
+            if url != nil && cookie.domain.hasSuffix(url!.host!) {
+                UserDefaults(suiteName: LDRUserDefaults.suiteName)?.set(cookie.value, forKey: LDRUserDefaults.session)
+                UserDefaults(suiteName: LDRUserDefaults.suiteName)?.synchronize()
+                //UserDefaults.standard.setValue(cookie.value, forKey: LDRUserDefaults.session)
+                //UserDefaults.standard.synchronize()
+            }
+        }
+    }
+
+    /**
+     * delete the cookie
+     * @param name cookie name
+     * @param domain cookie domain
+     */
+    func deleteCookie(name: String, domain: String) {
+        let cookies = HTTPCookieStorage.shared.cookies
+        if cookies == nil { return }
+
+        var deletingCookies: [HTTPCookie] = []
+        for cookie in cookies! {
+            if name != cookie.name { continue }
+            if domain != cookie.domain { continue }
+            deletingCookies.append(cookie)
+        }
+
+        for cookie in deletingCookies {
+            HTTPCookieStorage.shared.deleteCookie(cookie)
+        }
+    }
+
+    /**
+     * get string value or nil
+     * @param name cookie name
+     * @param domain cookie domain
+     * @return when it has value -> String value, when not -> nil
+     */
+    func value(name: String, domain: String) -> String? {
+        let cookies = HTTPCookieStorage.shared.cookies
+        if cookies == nil { return nil }
+
+        for cookie in cookies! {
+            if name != cookie.name { continue }
+            if domain != cookie.domain { continue }
+            return cookie.value
+        }
+
+        return nil
+    }
+
+}
