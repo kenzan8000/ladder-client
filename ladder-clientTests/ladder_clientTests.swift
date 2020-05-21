57b190011b98ffe3bf6320434f63e5e30c43ba19
kenzan8000 <kenzan8000@gmail.com>
2020-05-21 17:11:11 +0900

    add some async tests

--- ladder-clientTests/ladder_clientTests.swift
+++ ladder-clientTests/ladder_clientTests.swift
@@ -22,4 +22,83 @@ class ladder_clientTests: XCTestCase {
         }
         wait(for: [promise], timeout: 5)
     }
+    
+    func testRequestSubs() {
+        let promise = expectation(description: "Request Subs Succeeded")
+        LDRFeedOperationQueue.shared.requestSubs { (_, error) -> Void in
+            if let error = error {
+                XCTFail("Error: \(error.localizedDescription)")
+            } else {
+                promise.fulfill()
+            }
+        }
+        wait(for: [promise], timeout: 5)
+    }
+    
+    func testRequestUnread() {
+        let promise = expectation(description: "Request Unread Succeeded")
+        LDRFeedOperationQueue.shared.requestUnread(subscribeId: "1") { (_, error) -> Void in
+            if let error = error {
+                XCTFail("Error: \(error.localizedDescription)")
+            } else {
+                promise.fulfill()
+            }
+        }
+        wait(for: [promise], timeout: 5)
+    }
+    
+    func testRequestTouchAll() {
+        let promise = expectation(description: "Request Touch All Succeeded")
+        LDRFeedOperationQueue.shared.requestTouchAll(subscribeId: "1") { (_, error) -> Void in
+            if let error = error {
+                XCTFail("Error: \(error.localizedDescription)")
+            } else {
+                promise.fulfill()
+            }
+        }
+        wait(for: [promise], timeout: 5)
+    }
+    
+    func testRequestPinAdd() {
+        let promise = expectation(description: "Request Pin Add Succeeded")
+        guard let link = URL(string: "https://google.com") else {
+            return
+        }
+        let title = "Google"
+        LDRPinOperationQueue.shared.requestPinAdd(link: link, title: title) { _, error in
+            if let error = error {
+                XCTFail("Error: \(error.localizedDescription)")
+            } else {
+                promise.fulfill()
+            }
+        }
+        wait(for: [promise], timeout: 5)
+    }
+    
+    func testRequestPinRemove() {
+        let promise = expectation(description: "Request Pin Remove Succeeded")
+        guard let link = URL(string: "https://google.com") else {
+            return
+        }
+        LDRPinOperationQueue.shared.requestPinRemove(link: link) { _, error in
+            if let error = error {
+                XCTFail("Error: \(error.localizedDescription)")
+            } else {
+                promise.fulfill()
+            }
+        }
+        wait(for: [promise], timeout: 5)
+    }
+    
+    func testRequestPinAll() {
+        let promise = expectation(description: "Request Pin All Succeeded")
+        LDRPinOperationQueue.shared.requestPinAll { _, error in
+            if let error = error {
+                XCTFail("Error: \(error.localizedDescription)")
+            } else {
+                promise.fulfill()
+            }
+        }
+        wait(for: [promise], timeout: 5)
+    }
 }
