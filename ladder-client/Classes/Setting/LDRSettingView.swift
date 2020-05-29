419de807c3671aa3a9d475027f225e07687bb7cf
kenzan8000 <kenzan8000@gmail.com>
2020-05-29 17:56:50 +0900

    add -fprofile-instr-generate to other linker flags for swift ui preview

---
+++ ladder-client/Classes/Setting/LDRSettingView.swift
@@ -0,0 +1,13 @@
+import SwiftUI
+
+struct LDRSettingView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct LDRSettingView_Previews: PreviewProvider {
+    static var previews: some View {
+        LDRSettingView()
+    }
+}
