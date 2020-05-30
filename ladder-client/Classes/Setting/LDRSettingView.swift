4324d7b511a2157c11e2333802cea45d37716179
kenzan8000 <kenzan8000@gmail.com>
2020-05-30 18:00:31 +0900

    update LDRSettingView

--- ladder-client/Classes/Setting/LDRSettingView.swift
+++ ladder-client/Classes/Setting/LDRSettingView.swift
@@ -1,8 +1,44 @@
 import SwiftUI
 
 struct LDRSettingView: View {
+    @State private var urlDomain = ""
+    @State private var username = ""
+    @State private var password = ""
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack (alignment: .leading, spacing: 10) {
+            Text("URL")
+            HStack {
+                Text("https://")
+                    .padding(6)
+                    .background(Color.gray)
+                TextField("", text: $urlDomain)
+                    .textFieldStyle(RoundedBorderTextFieldStyle())
+            }
+            
+            Spacer()
+                .frame(height: 10)
+            
+            TextField("username", text: $username)
+                .textFieldStyle(RoundedBorderTextFieldStyle())
+            SecureField("password", text: $password)
+                .textFieldStyle(RoundedBorderTextFieldStyle())
+            
+            Spacer()
+                .frame(height: 10)
+            
+            HStack {
+                Button(
+                    action: { },
+                    label: { Text("Login") }
+                )
+                    .frame(width: 112)
+                    .padding(8)
+                    .border(Color.blue)
+            }
+            .frame(maxWidth: .infinity)
+        }
+        .padding(16)
     }
 }
 
