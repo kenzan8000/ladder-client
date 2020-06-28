fe579c615673498cef1f5266b3e5f0a8ce55a314
kenzan8000 <kenzan8000@gmail.com>
2020-06-28 22:13:50 +0900

    update to use scenedelegate and swiftui instead of appdelegate and storyboard

--- ladder-client/Classes/AppDelegate.swift
+++ ladder-client/Classes/AppDelegate.swift
@@ -5,7 +5,7 @@ import UIKit
 // MARK: - AppDelegate
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
-
+/*
     // MARK: - property
     var window: UIWindow?
 
@@ -58,5 +58,31 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func applicationWillTerminate(_ application: UIApplication) {
     }
+*/
 
+    // MARK: - lifecycle
+    
+    func application(
+        _ application: UIApplication,
+        didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
+    ) -> Bool {
+        true
+    }
+
+    func application(
+        _ application: UIApplication,
+        configurationForConnecting connectingSceneSession: UISceneSession,
+        options: UIScene.ConnectionOptions
+    ) -> UISceneConfiguration {
+        UISceneConfiguration(
+            name: "Default Configuration",
+            sessionRole: connectingSceneSession.role
+        )
+    }
+
+    func application(
+        _ application: UIApplication,
+        didDiscardSceneSessions sceneSessions: Set<UISceneSession>
+    ) {
+    }
 }
---
+++ ladder-client/Classes/SceneDelegate.swift
@@ -0,0 +1,68 @@
+import Firebase
+import KeychainAccess
+import SwiftUI
+import UIKit
+
+// MARK: - SceneDelegate
+class SceneDelegate: UIResponder, UIWindowSceneDelegate {
+    
+    // MARK: - property
+
+    var window: UIWindow?
+
+    // MARK: - life cycle
+
+    func scene(
+        _ scene: UIScene,
+        willConnectTo session: UISceneSession,
+        options connectionOptions: UIScene.ConnectionOptions
+    ) {
+        FirebaseApp.configure()
+
+        // remember last session
+        if let url = LDRRequestHelper.createUrl(path: LDR.login) {
+            if url.host != nil {
+                if let session = Keychain(
+                    service: LDRKeychain.serviceName,
+                    accessGroup: LDRKeychain.suiteName
+                    )[LDRKeychain.session] {
+
+                    let cookies = HTTPCookie.cookies(
+                        withResponseHeaderFields: ["Set-Cookie": "\(LDR.cookieName)=\(session)"],
+                        for: url
+                    )
+                    for cookie in cookies {
+                        HTTPCookieStorage.shared.setCookie(cookie)
+                    }
+                }
+            }
+        }
+        
+        let tabView = LDRTabView()
+
+        if let windowScene = scene as? UIWindowScene {
+            let window = UIWindow(windowScene: windowScene)
+            window.rootViewController = UIHostingController(rootView: tabView)
+            self.window = window
+            window.makeKeyAndVisible()
+        }
+    }
+
+    func sceneDidDisconnect(_ scene: UIScene) {
+    }
+
+    func sceneDidBecomeActive(_ scene: UIScene) {
+        NotificationCenter.default.post(name: LDRNotificationCenter.didBecomeActive, object: nil)
+    }
+
+    func sceneWillResignActive(_ scene: UIScene) {
+        NotificationCenter.default.post(name: LDRNotificationCenter.willResignActive, object: nil)
+    }
+
+    func sceneWillEnterForeground(_ scene: UIScene) {
+    }
+
+    func sceneDidEnterBackground(_ scene: UIScene) {
+    }
+
+}
