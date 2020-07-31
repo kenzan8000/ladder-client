05b3b6c1c36749df869fa25b5845bb5901ca42f0
kenzan8000 <kenzan8000@gmail.com>
2020-07-31 22:02:21 +0900

    add url domain test field test to LDRLoginViewTest

--- ladder-clientTests/Cases/AppModel/LDRLoginViewTest.swift
+++ ladder-clientTests/Cases/AppModel/LDRLoginViewTest.swift
@@ -1,4 +1,5 @@
 @testable import ladder_client
+@testable import ViewInspector
 import XCTest
 
 // MARK: - LDRLoginViewTest
@@ -7,11 +8,13 @@ class LDRLoginViewTest: XCTestCase {
     // MARK: - properties
     
     var sut: LDRLoginView?
+    var loginViewModel: LDRLoginViewModel?
     
     // MARK: - initialization
     
     override func setUpWithError() throws {
         sut = LDRLoginView()
+        loginViewModel = sut?.environmentObject(LDRLoginViewModel()) as? LDRLoginViewModel
         super.setUp()
     }
 
@@ -19,13 +22,17 @@ class LDRLoginViewTest: XCTestCase {
     
     override func tearDownWithError() throws {
         sut = nil
+        loginViewModel = nil
         super.tearDown()
     }
 
     // MARK: - tests
-    /*
+    
     /// Tests url domain text field
-    func testLoginView_whenInitialState_urlDomainTextField() {
+    func testLoginView_whenInitialState_urlDomainTextField() throws {
+        let text = try sut?.urlDomainTextField.inspect().textField().text().string()
+        print(text)
+        XCTAssertNil(text)
     }
-    */
+    
 }
