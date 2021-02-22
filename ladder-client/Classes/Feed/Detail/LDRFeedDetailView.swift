cdeb8bd10216cf13be0473cc3ca327e652e7f812
kenzan8000 <kenzan8000@gmail.com>
2021-02-22 20:36:23 +0900

    refactor feed detail's codes

--- ladder-client/Classes/Feed/Detail/LDRFeedDetailView.swift
+++ ladder-client/Classes/Feed/Detail/LDRFeedDetailView.swift
@@ -3,154 +3,150 @@ import SwiftUI
 
 // MARK: - LDRFeedDetailView
 struct LDRFeedDetailView: View {
-    // MARK: - property
+  // MARK: property
 
-    @ObservedObject var feedDetailViewModel: LDRFeedDetailViewModel
-    @ObservedObject var feedDetailWebViewModel: LDRFeedDetailWebViewModel
-    @Environment(\.colorScheme) var colorScheme: ColorScheme
+  @ObservedObject var feedDetailViewModel: LDRFeedDetailViewModel
+  @ObservedObject var feedDetailWebViewModel: LDRFeedDetailWebViewModel
+  @Environment(\.colorScheme) var colorScheme: ColorScheme
     
-    // MARK: - view
+  var body: some View {
+    VStack {
+      header
+      ZStack {
+        WebView(webView: feedDetailWebViewModel.webView)
+        if !feedDetailWebViewModel.doneInitialLoading {
+          ActivityIndicator(isAnimating: .constant(true), style: .large)
+        }
+      }
+      footer
+    }
+    .navigationBarTitle(feedDetailViewModel.unread.title)
+    .navigationBarItems(trailing: pinButton)
+    .sheet(isPresented: feedDetailWebViewModel.isPresentingSafariView) {
+      safariView
+    }
+    .onAppear {
+      feedDetailWebViewModel.loadHTMLString(
+        colorScheme: self.colorScheme,
+        body: self.feedDetailViewModel.body,
+        link: self.feedDetailViewModel.link
+      )
+    }
+    .alert(isPresented: feedDetailViewModel.isPresentingAlert) {
+      Alert(title: Text(feedDetailViewModel.error?.localizedDescription ?? ""))
+    }
+    .introspectViewController { viewController in
+      viewController.tabBarController?.tabBar.isHidden = true
+    }
+  }
 
-    var body: some View {
-        VStack {
-            header()
-            ZStack {
-                WebView(webView: feedDetailWebViewModel.webView)
-                if !feedDetailWebViewModel.doneInitialLoading {
-                    ActivityIndicator(
-                        isAnimating: .constant(true),
-                        style: .large
-                    )
-                }
-            }
-            footer()
+  var pinButton: some View {
+    Button(
+      action: {
+        if feedDetailViewModel.savePin() {
+          LDRToastView.show(
+            on: UIApplication.shared.windows[0],
+            text: "Added to Read Later Pins\n\(self.feedDetailViewModel.title)"
+          )
         }
-        .navigationBarTitle(feedDetailViewModel.unread.title)
-        .navigationBarItems(
-            trailing: pinButton()
-        )
-        .sheet(isPresented: feedDetailWebViewModel.isPresentingSafariView) {
-            self.safariView()
-        }
-        .onAppear {
-            self.feedDetailWebViewModel.loadHTMLString(
-                colorScheme: self.colorScheme,
-                body: self.feedDetailViewModel.body,
-                link: self.feedDetailViewModel.link
-            )
-        }
-        .alert(isPresented: feedDetailViewModel.isPresentingAlert) {
-          Alert(title: Text(feedDetailViewModel.error?.localizedDescription ?? ""))
-        }
-        .introspectViewController { viewController in
-            viewController.tabBarController?.tabBar.isHidden = true
-        }
-    }
-
-    func pinButton() -> some View {
-        Button(
-            action: {
-                if self.feedDetailViewModel.savePin() {
-                    LDRToastView.show(
-                        on: UIApplication.shared.windows[0],
-                        text: "Added to Read Later Pins\n\(self.feedDetailViewModel.title)"
-                    )
-                }
-            },
-            label: {
-                Text("Pin it")
-                .foregroundColor(.blue)
-                Image(systemName: "pin.fill")
-                .foregroundColor(.blue)
-            }
-        )
-    }
+      },
+      label: {
+        Text("Pin it")
+          .foregroundColor(.blue)
+        Image(systemName: "pin.fill")
+          .foregroundColor(.blue)
+      }
+    )
+  }
     
-    func header() -> some View {
-        Text(
-            "【\(feedDetailViewModel.index + 1) / \(feedDetailViewModel.count)】 \(feedDetailViewModel.title)"
-        )
-        .lineLimit(2)
-        .truncationMode(.tail)
-        .frame(minWidth: 0, maxWidth: .infinity, alignment: .leading)
-        .padding(EdgeInsets(top: 12, leading: 12, bottom: 0, trailing: 12))
-    }
+  var header: some View {
+    Text(
+      "【\(feedDetailViewModel.index + 1) / \(feedDetailViewModel.count)】 \(feedDetailViewModel.title)"
+    )
+    .lineLimit(2)
+    .truncationMode(.tail)
+    .frame(minWidth: 0, maxWidth: .infinity, alignment: .leading)
+    .padding(EdgeInsets(top: 12, leading: 12, bottom: 0, trailing: 12))
+  }
     
-    func footer() -> some View {
-        VStack {
-            HStack {
-                nextText(direction: -1)
-                nextText(direction: 1)
-            }
-            .padding(EdgeInsets(top: 0, leading: 0, bottom: 0, trailing: 0))
-            HStack {
-                moveButton(direction: -1)
-                moveButton(direction: 1)
-            }
-            .padding(EdgeInsets(top: 12, leading: 0, bottom: 24, trailing: 0))
-        }
+  var footer: some View {
+    VStack {
+      HStack {
+        nextText(direction: -1)
+        nextText(direction: 1)
+      }
+        .padding(EdgeInsets(top: 0, leading: 0, bottom: 0, trailing: 0))
+      HStack {
+        moveButton(direction: -1)
+        moveButton(direction: 1)
+      }
+        .padding(EdgeInsets(top: 12, leading: 0, bottom: 24, trailing: 0))
     }
+  }
     
-    func nextText(direction: Int) -> some View {
-        var text = direction < 0 ? feedDetailViewModel.prevTitle : feedDetailViewModel.nextTitle
-        if !text.isEmpty {
-            text = "【\(feedDetailViewModel.index + 1 + direction)】 " + text
-        }
-        return Text(text)
-        .lineLimit(2)
-        .truncationMode(.tail)
-        .frame(minWidth: 0, maxWidth: .infinity, alignment: .leading)
-        .padding(EdgeInsets(top: 0, leading: 12, bottom: 0, trailing: 12))
+  var safariView: some View {
+    guard let url = feedDetailWebViewModel.safariUrl else {
+      return AnyView(EmptyView())
     }
+    return AnyView(SafariView(url: url))
+  }
+  
+  // MARK: public api
+  
+  func nextText(direction: Int) -> some View {
+    var text = direction < 0 ? feedDetailViewModel.prevTitle : feedDetailViewModel.nextTitle
+    if !text.isEmpty {
+      text = "【\(feedDetailViewModel.index + 1 + direction)】 " + text
+    }
+    return Text(text)
+      .lineLimit(2)
+      .truncationMode(.tail)
+      .frame(minWidth: 0, maxWidth: .infinity, alignment: .leading)
+      .padding(EdgeInsets(top: 0, leading: 12, bottom: 0, trailing: 12))
+  }
     
-    func moveButton(direction: Int) -> some View {
-        var color = Color.blue
-        var opacity = 1.0
-        if feedDetailViewModel.index + direction < 0 || feedDetailViewModel.index + direction >= feedDetailViewModel.count {
-            color = Color.blue
-            opacity = 0.5
-        }
-        return Button(
-            action: {
-                if !self.feedDetailViewModel.move(offset: direction) {
-                    LDRBlinkView.show(
-                        on: UIApplication.shared.windows[0],
-                        color: UIColor.systemGray6.withAlphaComponent(0.5),
-                        count: 1,
-                        interval: 0.08
-                    )
-                } else {
-                    self.feedDetailWebViewModel.loadHTMLString(
-                        colorScheme: self.colorScheme,
-                        body: self.feedDetailViewModel.body,
-                        link: self.feedDetailViewModel.link
-                    )
-                }
-            },
-            label: {
-                Image(systemName: direction == -1 ? "chevron.left" : "chevron.right")
-                .font(.title)
-                .foregroundColor(color)
-            }
-        )
-        .frame(minWidth: 0, maxWidth: .infinity, alignment: .center)
-        .opacity(opacity)
+  func moveButton(direction: Int) -> some View {
+    var color = Color.blue
+    var opacity = 1.0
+    if feedDetailViewModel.index + direction < 0 || feedDetailViewModel.index + direction >= feedDetailViewModel.count {
+      color = Color.blue
+      opacity = 0.5
     }
-    
-    func safariView() -> some View {
-        guard let url = self.feedDetailWebViewModel.safariUrl else {
-            return AnyView(EmptyView())
+    return Button(
+      action: {
+        if !self.feedDetailViewModel.move(offset: direction) {
+          LDRBlinkView.show(
+            on: UIApplication.shared.windows[0],
+            color: UIColor.systemGray6.withAlphaComponent(0.5),
+            count: 1,
+            interval: 0.08
+          )
+        } else {
+          self.feedDetailWebViewModel.loadHTMLString(
+            colorScheme: self.colorScheme,
+            body: self.feedDetailViewModel.body,
+            link: self.feedDetailViewModel.link
+          )
         }
-        return AnyView(SafariView(url: url))
-    }
+      },
+      label: {
+        Image(systemName: direction == -1 ? "chevron.left" : "chevron.right")
+          .font(.title)
+          .foregroundColor(color)
+      }
+    )
+      .frame(minWidth: 0, maxWidth: .infinity, alignment: .center)
+      .opacity(opacity)
+  }
+ 
 }
 
 // MARK: - LDRFeedDetailView_Previews
 struct LDRFeedDetailView_Previews: PreviewProvider {
-    static var previews: some View {
-        LDRFeedDetailView(
-            feedDetailViewModel: LDRFeedDetailViewModel(unread: LDRFeedUnread(subscribeId: "1", title: "Title")),
-            feedDetailWebViewModel: LDRFeedDetailWebViewModel()
-        )
-    }
+  static var previews: some View {
+    LDRFeedDetailView(
+      feedDetailViewModel: LDRFeedDetailViewModel(unread: LDRFeedUnread(subscribeId: "1", title: "Title")),
+      feedDetailWebViewModel: LDRFeedDetailWebViewModel()
+    )
+  }
 }
--- ladder-client/Classes/Feed/Detail/LDRFeedDetailViewModel.swift
+++ ladder-client/Classes/Feed/Detail/LDRFeedDetailViewModel.swift
@@ -6,42 +6,15 @@ import WebKit
 // MARK: - LDRFeedDetailViewModel
 final class LDRFeedDetailViewModel: ObservableObject {
     
-    // MARK: - model
-    @Published var unread: LDRFeedUnread
-    @Published var index: Int = 0
-    var count: Int {
-        unread.items.count
-    }
-    var title: String {
-        guard let title = unread.getTitle(at: index) else {
-            return ""
-        }
-        return title
-    }
-    var body: String {
-        guard let body = unread.getBody(at: index) else {
-            return ""
-        }
-        return body
-    }
-    var link: URL {
-        guard let link = unread.getLink(at: index) else {
-            return URL(fileURLWithPath: "")
-        }
-        return link
-    }
-    var prevTitle: String {
-        guard let title = unread.getTitle(at: index - 1) else {
-            return ""
-        }
-        return title
-    }
-    var nextTitle: String {
-        guard let title = unread.getTitle(at: index + 1) else {
-            return ""
-        }
-        return title
-    }
+  // MARK: property
+  @Published var unread: LDRFeedUnread
+  @Published var index: Int = 0
+  var count: Int { unread.items.count }
+  var title: String { unread.getTitle(at: index) ?? "" }
+  var body: String { unread.getBody(at: index) ?? "" }
+  var link: URL { unread.getLink(at: index) ?? URL(fileURLWithPath: "") }
+  var prevTitle: String { unread.getTitle(at: index - 1) ?? "" }
+  var nextTitle: String { unread.getTitle(at: index + 1) ?? "" }
   @Published var error: Error?
   var isPresentingAlert: Binding<Bool> {
     Binding<Bool>(
@@ -57,74 +30,60 @@ final class LDRFeedDetailViewModel: ObservableObject {
   private var pinAddCancellables = Set<AnyCancellable>()
   private var notificationCancellables = Set<AnyCancellable>()
 
-    // MARK: - initialization
+  // MARK: initialization
     
-    init(unread: LDRFeedUnread) {
-        self.unread = unread
-        NotificationCenter.default.publisher(for: LDRNotificationCenter.didLogin)
-          .receive(on: DispatchQueue.main)
-          .sink { [weak self] _ in
-            self?.pinAddCancellables.forEach { $0.cancel() }
+  init(unread: LDRFeedUnread) {
+    self.unread = unread
+    NotificationCenter.default.publisher(for: LDRNotificationCenter.didLogin)
+      .receive(on: DispatchQueue.main)
+      .sink { [weak self] _ in
+        self?.pinAddCancellables.forEach { $0.cancel() }
+      }
+      .store(in: &notificationCancellables)
+  }
+
+  // MARK: public api
+    
+  /// Update unread index to next or previous unread
+  /// - Parameter offset: offset to move 1 or -1
+  /// - Returns: Bool if you can move
+  func move(offset: Int) -> Bool {
+    index += offset
+    if index < 0 {
+      index = 0
+      return false
+    } else if index >= count {
+      index = count - 1
+      return false
+    }
+    return true
+  }
+
+  /// Save pin you currently focus on
+  /// - Returns: Bool if you could save the current focusing pin on local DB
+  func savePin() -> Bool {
+    let url = link
+    if LDRPin.exists(link: url.absoluteString, title: title) {
+      return true
+    }
+    if LDRPin.saveByAttributes(createdOn: "", title: title, link: url.absoluteString) != nil {
+      return false
+    }
+    URLSession.shared.publisher(for: .pinAdd(link: url, title: title))
+      .receive(on: DispatchQueue.main)
+      .sink(
+        receiveCompletion: { [weak self] result in
+          if case let .failure(error) = result {
+            self?.error = error
           }
-          .store(in: &notificationCancellables)
-    }
-
-    // MARK: - destruction
-
-    deinit {
-    }
-
-    // MARK: - access to the model
-
-    // MARK: - intent
-    
-    // MARK: - notification
-    
-    // MARK: - public api
-    
-    /// Update unread index to next or previous unread
-    /// - Parameter offset: offset to move 1 or -1
-    /// - Returns: Bool if you can move
-    func move(offset: Int) -> Bool {
-        index += offset
-        if index < 0 {
-            index = 0
-            return false
-        } else if index >= count {
-            index = count - 1
-            return false
+        },
+        receiveValue: { [weak self] response in
+          if !response.isSuccess {
+            self?.error = LDRError.failed("Failed to remove a pin. (\(url.absoluteString))")
+          }
         }
-        return true
-    }
-
-    /// Save pin you currently focus on
-    /// - Returns: Bool if you could save the current focusing pin on local DB
-    func savePin() -> Bool {
-        let url = link
-        if LDRPin.exists(link: url.absoluteString, title: title) {
-          return true
-        }
-        if LDRPin.saveByAttributes(createdOn: "", title: title, link: url.absoluteString) != nil {
-            return false
-        }
-        URLSession.shared.publisher(for: .pinAdd(link: url, title: title))
-          .receive(on: DispatchQueue.main)
-          .sink(
-            receiveCompletion: { [weak self] result in
-              if case let .failure(error) = result {
-                self?.error = error
-              }
-            },
-            receiveValue: { [weak self] response in
-              if !response.isSuccess {
-                self?.error = LDRError.failed("Failed to remove a pin. (\(url.absoluteString))")
-              }
-            }
-          )
-          .store(in: &pinAddCancellables)
-        return true
-    }
-    
-    // MARK: - private api
-
+      )
+      .store(in: &pinAddCancellables)
+    return true
+  }
 }
