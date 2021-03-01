6a89535596436b40be187c899006ec889d18e6ba
kenzan8000 <kenzan8000@gmail.com>
2021-03-01 14:31:18 +0900

    refactor coredata manager

--- ladder-client/Classes/CoreData/LDRCoreDataManager.swift
+++ ladder-client/Classes/CoreData/LDRCoreDataManager.swift
@@ -3,48 +3,23 @@ import CoreData
 // MARK: - LDRCoreDataManager
 class LDRCoreDataManager {
 
-  // MARK: class method
+  // MARK: static property
   static let shared = LDRCoreDataManager()
 
   // MARK: property
-  var managedObjectModel: NSManagedObjectModel? {
-    guard let modelURL = Bundle.main.url(forResource: "LDRModel", withExtension: "momd") else {
-      return nil
-    }
-    return NSManagedObjectModel(contentsOf: modelURL)
-  }
-
-  private static var ldr_managedObjectContext: NSManagedObjectContext?
+  let persistentContainer: NSPersistentContainer
   var managedObjectContext: NSManagedObjectContext {
-    if let context = LDRCoreDataManager.ldr_managedObjectContext {
-      return context
-    }
-    let coordinator = self.persistentStoreCoordinator
-    let managedObjectContext = NSManagedObjectContext(concurrencyType: .mainQueueConcurrencyType)
-    managedObjectContext.persistentStoreCoordinator = coordinator
-    LDRCoreDataManager.ldr_managedObjectContext = managedObjectContext
-    return managedObjectContext
+    persistentContainer.viewContext
   }
-
-  var persistentStoreCoordinator: NSPersistentStoreCoordinator? {
-    let documentsDirectories = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask)
-    let documentsDirectory = documentsDirectories[documentsDirectories.count - 1] as NSURL
-    let storeURL = documentsDirectory.appendingPathComponent("LDRModel.sqlite")
-    guard let mom = self.managedObjectModel else {
-      return nil
+  
+  // MARK: initialization
+  init() {
+    persistentContainer = NSPersistentContainer(name: "LDRModel")
+    persistentContainer.loadPersistentStores { _, error in
+      if let error = error {
+        fatalError("Core Data store failed to load with error: \(error)")
+      }
     }
-    let persistentStoreCoordinator = NSPersistentStoreCoordinator(managedObjectModel: mom)
-    do {
-      let options = [NSInferMappingModelAutomaticallyOption: true, NSMigratePersistentStoresAutomaticallyOption: true]
-      try persistentStoreCoordinator.addPersistentStore(
-        ofType: NSSQLiteStoreType,
-        configurationName: nil,
-        at: storeURL,
-        options: options
-      )
-    } catch {
-    }
-    return persistentStoreCoordinator
   }
 
 }
