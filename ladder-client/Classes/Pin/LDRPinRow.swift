9de4a9d0a849909160bceda7a3b5f9a6bb72a33f
kenzan8000 <kenzan8000@gmail.com>
2021-03-07 21:01:48 +0900

    add LDRPinRowTests

--- ladder-client/Classes/Pin/LDRPinRow.swift
+++ ladder-client/Classes/Pin/LDRPinRow.swift
@@ -16,11 +16,13 @@ struct LDRPinRow: View {
       label: {
         Text(title)
           .fixedSize(horizontal: false, vertical: true)
-          .frame(height: 64)
+          .frame(minWidth: 0, maxWidth: .infinity, minHeight: 64, alignment: .leading)
+          .padding(.horizontal, 16.0)
           .lineLimit(2)
           .truncationMode(.tail)
       }
     )
+    .frame(minWidth: 0, maxWidth: .infinity)
   }
   
   // MARK: public api
@@ -32,6 +34,20 @@ struct LDRPinRow: View {
 
 // MARK: - LDRPinRow_Previews
 struct LDRPinRow_Previews: PreviewProvider {
+  static var previews: some View {
+    LDRPinRow(title: "Rails アプリでオンラインでカラムの削除やリネームを行うには - eagletmt's blog")
+  }
+}
+
+// MARK: - LDRPinRowShortTitle_Previews
+struct LDRPinRowShortTitle_Previews: PreviewProvider {
+  static var previews: some View {
+    LDRPinRow(title: "Rails - eagletmt's blog")
+  }
+}
+
+// MARK: - LDRPinRowLongTitle_Previews
+struct LDRPinRowLongTitle_Previews: PreviewProvider {
   static var previews: some View {
     LDRPinRow(title: "Rails アプリでオンラインでカラムの削除やリネームを行うには - eagletmt's blog あいうえお かきくけこ さしすせそ たちつてと あいうえお かきくけこ さしすせそ たちつてと")
   }
---
+++ ladder-clientTests/Pin/LDRPinRowTests.swift
@@ -0,0 +1,66 @@
+import SnapshotTesting
+import SwiftUI
+import XCTest
+@testable import ladder_client
+
+// MARK: - LDRPinRowTests
+class LDRPinRowTests: XCTestCase {
+
+  // MARK: life cycle
+  
+  override func setUpWithError() throws {
+    super.setUp()
+  }
+
+  override func tearDownWithError() throws {
+    super.tearDown()
+  }
+
+  // MARK: test
+  
+  func testLDRPinRow_whenInitialState_snapshotTesting() throws {
+    let sut = UIHostingController(
+      rootView: LDRPinRow(title: "Rails アプリでオンラインでカラムの削除やリネームを行うには - eagletmt's blog")
+    )
+    
+    [(UIUserInterfaceStyle.dark, "dark"), (UIUserInterfaceStyle.light, "light")].forEach { style, named in
+      sut.overrideUserInterfaceStyle = style
+      assertSnapshot(
+        matching: sut,
+        as: .image,
+        named: named
+      )
+    }
+  }
+  
+  func testLDRPinRow_whenShortTitle_snapshotTesting() throws {
+    let sut = UIHostingController(
+      rootView: LDRPinRow(title: "Rails - eagletmt's blog")
+    )
+    
+    [(UIUserInterfaceStyle.dark, "dark"), (UIUserInterfaceStyle.light, "light")].forEach { style, named in
+      sut.overrideUserInterfaceStyle = style
+      assertSnapshot(
+        matching: sut,
+        as: .image,
+        named: named
+      )
+    }
+  }
+  
+  func testLDRPinRow_whenLongTitle_snapshotTesting() throws {
+    let sut = UIHostingController(
+      rootView: LDRPinRow(title: "Rails アプリでオンラインでカラムの削除やリネームを行うには - eagletmt's blog あいうえお かきくけこ さしすせそ たちつてと あいうえお かきくけこ さしすせそ たちつてと")
+    )
+    
+    [(UIUserInterfaceStyle.dark, "dark"), (UIUserInterfaceStyle.light, "light")].forEach { style, named in
+      sut.overrideUserInterfaceStyle = style
+      assertSnapshot(
+        matching: sut,
+        as: .image,
+        named: named
+      )
+    }
+  }
+
+}
