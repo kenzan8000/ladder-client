8aa153b31de430e116a0fbb599a9263744964d00
kenzan8000 <kenzan8000@gmail.com>
2021-03-14 19:55:54 +0900

    add feed detail view snapshot testing

---
+++ ladder-clientSnapshotTests/Feed/Detail/LDRFeedDetailViewTests.swift
@@ -0,0 +1,44 @@
+import SnapshotTesting
+import SwiftUI
+import XCTest
+@testable import ladder_client
+
+// MARK: - LDRFeedDetailViewTests
+class LDRFeedDetailViewTests: XCTestCase {
+
+  // MARK: life cycle
+  
+  override func setUpWithError() throws {
+    super.setUp()
+  }
+
+  override func tearDownWithError() throws {
+    super.tearDown()
+  }
+
+  // MARK: test
+  
+  func testLDRFeedDetailView_whenInitialState_snapshotTesting() throws {
+    let storageProvider = LDRStorageProvider(source: Bundle(for: type(of: LDRFeedDetailViewTests())), name: LDR.coreData, group: LDR.testGroup)
+    let subsunreads = storageProvider.fetchSubsUnreads(by: .rate)
+    let sut = UIHostingController(
+      rootView: LDRFeedDetailView(
+        feedDetailViewModel: LDRFeedDetailViewModel(
+          storageProvider: storageProvider,
+          subsunread: subsunreads[0]
+        ),
+        feedDetailWebViewModel: LDRFeedDetailWebViewModel()
+      )
+    )
+    
+    [(UIUserInterfaceStyle.dark, "dark"), (UIUserInterfaceStyle.light, "light")].forEach { style, named in
+      sut.overrideUserInterfaceStyle = style
+      assertSnapshot(
+        matching: sut,
+        as: .image(on: .iPhone8, precision: 0.98, traits: .iPhone8(.portrait)),
+        named: named
+      )
+    }
+  }
+
+}
