f688b5ebb97da4b15986e840be8bbcd6098dbcf1
kenzan8000 <me@kenzan8000.org>
2021-08-23 16:25:58 +0900

    add Alert+ViewModel

---
+++ ladder-client/Classes/Extension/SwiftUI/Alert+ViewModel.swift
@@ -0,0 +1,12 @@
+import SwiftUI
+
+// MARK: - Alert + ViewModel
+extension Alert {
+  struct ViewModel: Identifiable {
+    let title: String
+    let message: String
+    let buttonText: String
+    let buttonAction: (() -> Void)?
+    var id: String { title + message + buttonText }
+  }
+}
