a196b961976619a47e4b782e2404fa538d5822ba
kenzan8000 <me@kenzan8000.org>
2021-09-09 00:13:49 +0900

    update LDRFeedDetailViewTests snapshot test timeout

--- ladder-clientSnapshotTests/Feed/Detail/LDRFeedDetailViewTests.swift
+++ ladder-clientSnapshotTests/Feed/Detail/LDRFeedDetailViewTests.swift
@@ -41,7 +41,8 @@ class LDRFeedDetailViewTests: XCTestCase {
       assertSnapshot(
         matching: sut,
         as: .img(precision: 0.98),
-        named: named + "." + model.name
+        named: named + "." + model.name,
+        timeout: 10.0
       )
     }
   }
