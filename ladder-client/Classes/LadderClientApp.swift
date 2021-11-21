842c4505b9e71f7010aa9d2c2dd8d9297c1060ce
kenzan8000 <me@kenzan8000.org>
2021-11-21 19:51:30 +0900

    add launch time ui test

--- ladder-client/Classes/LadderClientApp.swift
+++ ladder-client/Classes/LadderClientApp.swift
@@ -29,8 +29,8 @@ struct LadderClientApp: App {
       LDRTabView(
         keychain: keychain,
         selected: LDRTabView.Tab.feed,
-        feedViewModel: LDRFeedView.ViewModel(storageProvider: storageProvider, keychain: keychain, segment: .rate),
-        pinViewModel: LDRPinView.ViewModel(storageProvider: storageProvider, keychain: keychain)
+        feedViewModel: .init(storageProvider: storageProvider, keychain: keychain, segment: .rate),
+        pinViewModel: .init(storageProvider: storageProvider, keychain: keychain)
       )
       .environmentObject(LDRLoginView.ViewModel(keychain: keychain))
     }
